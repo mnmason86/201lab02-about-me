f3b7951376ad86853356c290edb50a9b5b9bc059
Mandy Mason <mnmason86@gmail.com>
2022-05-04 14:48:52 -0400

    began creating about me game

---
+++ js/app.js
@@ -0,0 +1,9 @@
+
+let userName = prompt('What is your name?');
+alert ('Welcome to my About Me page, ' + userName + '.');
+//console.log(userName);
+
+let questionOne = prompt('In what city was Mandy born?');
+if (questionOne.toLowerCase == 'nashville'){
+  alert ('Yes! Mandy was born in Nashville!');
+} 
